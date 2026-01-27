2b3cd6c78832a7d2b9d7fc3cfdd77385bbd35a1a
slav <vrazumihindima@gmail.com>
2026-01-27 21:28:58 +0300

    serializers.py changes

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,20 +1,36 @@
 from rest_framework import serializers
 
 from .models import Course, Lesson
+from .validators import validate_youtube_only
+from .models import Subscription  # добавим модель в задании 2
+
 
 class LessonSerializer(serializers.ModelSerializer):
+    video_url = serializers.URLField(
+        required=False,
+        allow_blank=True,
+        validators=[validate_youtube_only],
+    )
+
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = "__all__"
+
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
-    lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
+    lessons = LessonSerializer(source="lessons", many=True, read_only=True)
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = "__all__"
 
     def get_lessons_count(self, obj):
-        return obj.lesson_set.count()
+        return obj.lessons.count()
 
+    def get_is_subscribed(self, obj):
+        request = self.context.get("request")
+        if not request or request.user.is_anonymous:
+            return False
+        return Subscription.objects.filter(user=request.user, course=obj).exists()
