5bd7edbc80edce28593d61e15ff3e493f199555d
slav <vrazumihindima@gmail.com>
2026-02-09 13:59:22 +0300

    small fixes

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 AUTH_USER_MODEL = 'users.User'
 MEDIA_URL = '/media/'
